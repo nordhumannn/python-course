9f949778179a03306d7c743316ca82bed78a4ee0
nordhumannn <97041147+nordhumannn@users.noreply.github.com>
2022-05-20 19:41:27 +0300

    Create lection3.py

---
+++ lection3.py
@@ -0,0 +1,29 @@
+#1
+apart = int(input("Номер квартиры: "))
+floor = 9
+entrance = 4
+num = 4
+
+total_apart = floor * num
+entrance_res = (apart - 1) // total_apart + 1
+floor_res = (apart % total_apart) // 4 + 1
+
+print(entrance_res, floor_res)
+
+#2
+year = int(input("Enter a year: "))
+
+if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    print(f"{year}: высокосный год")
+else:
+    print(f"{year}: не высокосный год")
+
+#3  
+side_a = int(input("Enter a value of side A: "))
+side_b = int(input("Enter a value of side B: "))
+side_c = int(input("Enter a value of side C: "))
+
+if (side_a + side_b) > side_c and (side_a + side_c) > side_b and (side_c + side_b) > side_a:
+    print("This triangle exists")
+else:
+    print("This triangle cannot exist")
