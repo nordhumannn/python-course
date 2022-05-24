f3e1b74a29656c983a02f71c33c78d72907a34b8
nordhumannn <97041147+nordhumannn@users.noreply.github.com>
2022-05-24 16:21:35 +0300

    Update lection3.py

--- lection3.py
+++ lection3.py
@@ -1,14 +1,28 @@
+from math import ceil
+
 #1
 apart = int(input("Номер квартиры: "))
-floor = 9
-entrance = 4
-num = 4
+floor, entrance, num = 9, 4, 4
 
 total_apart = floor * num
 entrance_res = (apart - 1) // total_apart + 1
 floor_res = (apart % total_apart) // 4 + 1
 
 print(entrance_res, floor_res)
+
+#1.1
+n = int(input('>>> '))
+
+if n <= 4 * 9:
+    print(f'Flat: {n} is on the entrance 1, floor: {ceil(n / 4)}')
+elif 4 * 9 < n <= 4 * 9 * 2:
+    print(f'Flat: {n} is on the entrance 2, floor: {ceil(n / 4) - 9}')
+elif 4 * 9 * 2 < n <= 4 * 9 * 3:
+    print(f'Flat: {n} is on the entrance 3, floor: {ceil(n / 4) - 18}')
+elif 4 * 9 * 3 < n <= 4 * 9 * 4:
+    print(f'Flat: {n} is on the entrance 4, floor: {ceil(n / 4) - 27}')
+else:
+    print('There is no such flat')
 
 #2
 year = int(input('Enter a year: '))
@@ -19,11 +33,7 @@ else:
     print(f"{year}: не высокосный год")
 
 #3  
-side_a = int(input("Enter a value of side A: "))
-side_b = int(input("Enter a value of side B: "))
-side_c = int(input("Enter a value of side C: "))
+a, b, c = map(int, input('Enter the value of the sides(3): ').split(','))
+res = True if a + b > c and b + c > a and a + c > b else False
 
-if (side_a + side_b) > side_c and (side_a + side_c) > side_b and (side_c + side_b) > side_a:
-    print("This triangle exists")
-else:
-    print("This triangle cannot exist")
+print(res)
