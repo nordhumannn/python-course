5749cf162644c422d477700494a27e3ff9d28ccd
nordhumannn <97041147+nordhumannn@users.noreply.github.com>
2022-05-24 16:24:34 +0300

    Update lection3.py

--- lection3.py
+++ lection3.py
@@ -1,26 +1,26 @@
 from math import ceil
 
 #1
-apart = int(input("Номер квартиры: "))
+apart = int(input('Flat: '))
 floor, entrance, num = 9, 4, 4
 
 total_apart = floor * num
 entrance_res = (apart - 1) // total_apart + 1
 floor_res = (apart % total_apart) // 4 + 1
 
-print(entrance_res, floor_res)
+print(f'Entrance: {entrance_res}, floor: {floor_res}')
 
 #1.1
 n = int(input('>>> '))
 
 if n <= 4 * 9:
-    print(f'Flat: {n} is on the entrance 1, floor: {ceil(n / 4)}')
+    print(f'Flat: {n}, entrance 1, floor: {ceil(n / 4)}')
 elif 4 * 9 < n <= 4 * 9 * 2:
-    print(f'Flat: {n} is on the entrance 2, floor: {ceil(n / 4) - 9}')
+    print(f'Flat: {n}, entrance 2, floor: {ceil(n / 4) - 9}')
 elif 4 * 9 * 2 < n <= 4 * 9 * 3:
-    print(f'Flat: {n} is on the entrance 3, floor: {ceil(n / 4) - 18}')
+    print(f'Flat: {n}, entrance 3, floor: {ceil(n / 4) - 18}')
 elif 4 * 9 * 3 < n <= 4 * 9 * 4:
-    print(f'Flat: {n} is on the entrance 4, floor: {ceil(n / 4) - 27}')
+    print(f'Flat: {n}, entrance 4, floor: {ceil(n / 4) - 27}')
 else:
     print('There is no such flat')
 
