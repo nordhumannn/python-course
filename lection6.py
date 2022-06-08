dbf311591ebdd2cdcbb8977b2842de63943fbd35
nordhumannn <97041147+nordhumannn@users.noreply.github.com>
2022-06-08 11:27:04 +0300

    Update lection6.py

--- lection6.py
+++ lection6.py
@@ -1,4 +1,5 @@
-# 1
+#------------- 1 -------------
+
 s = input('>>> ')
 res = 0
 
@@ -13,13 +14,14 @@ print(res)
 s = input('>>> ')
 print(s.count('b'))
 
-# 2
+#------------- 2 -------------
+
 name = input('Name: ')
 res = 'Valid' if name.istitle() and name.isalpha() else 'NOT valid'
 
 print(res)
+#------------- 3 -------------
 
-# 3
 s = input('>>> ')
 res = 0
 
@@ -28,12 +30,45 @@ for item in s:
 
 print(res)
 
-# 4
+#------------- 4 -------------
+
 import math
 
 for n in range(2, 12):
     print(f'{math.pi:.{n}f}')
 
-# 5
+#------------- 5 -------------
+    
 s = input('>>> ')
 print(max(s.split(), key=len))
+
+#------------- 6 -------------
+
+s = input('>>> ')
+
+  for i in range(1, len(s) // 2):
+  pattern = s[:i]
+  if s.count(pattern) * len(pattern) == len(s):
+      print(pattern)
+      break
+else:
+  print('Error')
+
+#------------- 7 -------------
+
+tags = ['p', 'a', 'h1', 'h2']
+
+html_text = '''
+<a parameters>Python is the best programming language</a>
+<p parameters>Some text text text text text text <a parameters>LINK</a>
+'''
+
+for tag in tags:
+    while f'<{tag}' in html_text:
+        start = html_text.find(f'<{tag}') 
+        stop = html_text.find('>', start + 1)
+
+        html_text = html_text.replace(html_text[start : stop + 1], '')
+    html_text = html_text.replace(f'</{tag}>', '')
+
+print(html_text)
