c1a38cf088a6afba9cb26e9db00a84109b70194a
nordhumannn <97041147+nordhumannn@users.noreply.github.com>
2022-06-28 19:59:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ from product import Product
 from customer import Customer
 from order import Order
 
-product_1 = Product(199, 'XboX gamepad', '15x20')
-product_2 = Product(350, 'Keyboard', '40X20')
+pr_1 = Product(20, 'Apple')
+pr_2 = Product(30, 'Banana')
 
-customer_1 = Customer('John', 'Doe', '+2948239842')
+cust_1 = Customer('Andrew', '+1234567890')
+cust_2 = Customer('John', '+0987654321')
 
-order_1 = Order(customer_1, product_1, product_2)
+order_1 = Order(cust_1)
+order_2 = Order(cust_2)
+
+order_1.add_product(pr_1, 2)
+order_1.add_product(pr_2, 2)
+order_1.add_product(pr_1, 1)
+
+order_2.add_product(pr_1, 3)
 
 print(order_1)
