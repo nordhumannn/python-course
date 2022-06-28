5347c8cb318ebfead3aecc3c33daea8b3143dea0
nordhumannn <97041147+nordhumannn@users.noreply.github.com>
2022-06-28 20:00:46 +0300

    Update product.py

--- product.py
+++ product.py
@@ -1,11 +1,8 @@
 class Product:
-    def __init__(self, price, description, size):
+    
+    def __init__(self, price: int | float, description: str):
         self.price = price
         self.description = description
-        self.size = size
-
-    def get_price(self):
-        return self.price
 
     def __str__(self) -> str:
-        return f'{self.price}, {self.description} {self.size}'
+        return f'{self.description} - {self.price}'
