dbb7617362372588e594342ccfca10d326944611
nordhumannn <97041147+nordhumannn@users.noreply.github.com>
2022-07-05 16:23:13 +0300

    Update exception.py

--- exception.py
+++ exception.py
@@ -1,10 +1,21 @@
 class NegativeValue(Exception):
 
     def __init__(self, msg):
+        super().__init__()
         self.msg = msg
 
     def return_exception(self):
         return self.msg
+
+class Product:
+    
+    def __init__(self, price: int | float, description: str):
+        super().__init__()
+        self.price = price
+        self.description = description
+
+    def __str__(self) -> str:
+        return f'{self.description} - {self.price}'
 
 while True:
     try:
@@ -12,7 +23,10 @@ while True:
         if price < 0:
             raise NegativeValue("Price can't be negative, please input positive number")
         break
-    except ValueError as error:
-        print("Integers only\n", error)
-    except NegativeValue as error:
-            print(error.return_exception())
+    except ValueError:
+        print("ValueError: Only integers acceptable")
+    except NegativeValue as err:
+        print(err.return_exception())
+
+pr_1 = Product(price, 'Apple')
+print(pr_1)
