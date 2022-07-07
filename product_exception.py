cfdf938cc46e9cbc4f508afbe773b76113f7d1bb
nordhumannn <97041147+nordhumannn@users.noreply.github.com>
2022-07-07 20:23:39 +0300

    Update product_exception.py

--- product_exception.py
+++ product_exception.py
@@ -1,32 +1,25 @@
 class NegativeValue(Exception):
-
-    def __init__(self, msg):
+    
+    def __init__(self, value: int | float=None, msg: str=None):
         super().__init__()
+        self.value = value
         self.msg = msg
 
-    def return_exception(self):
-        return self.msg
+    def __str__(self):
+        return f'{self.value}, {self.msg}'
 
 class Product:
     
     def __init__(self, price: int | float, description: str):
-        super().__init__()
+
+        if not isinstance(price, (int, float)):
+            raise TypeError()
+
+        if price <= 0:
+            raise NegativeValue(price, 'price attribute must be positive')
+
         self.price = price
         self.description = description
 
     def __str__(self) -> str:
         return f'{self.description} - {self.price}'
-
-while True:
-    try:
-        price = float(input("Input price: "))
-        if price < 0:
-            raise NegativeValue("Price can't be negative, please input positive number")
-        break
-    except ValueError:
-        print("ValueError: Only integers acceptable")
-    except NegativeValue as err:
-        print(err.return_exception())
-
-pr_1 = Product(price, 'Apple')
-print(pr_1)
